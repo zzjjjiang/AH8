3d6e213bc9a5dadacb89e7c8ed553ce2c1b3761a
Dhruv T <dhruv.thota@fiserv.com>
2023-11-03 13:03:12 -0400

    initalizing repo

---
+++ ECSource.java
@@ -0,0 +1,29 @@
+class Node<T> {
+  T val;
+  Node<T> next;
+  
+  public Node(T val) {
+    this.val = val;
+    this.next = null;
+  }
+}
+
+class ECSource {
+  public static <T> T getNodeValue(Node<T> head, int index) {
+    // todo - note the return type, but don't overthink it
+    return;
+  }
+  
+  public static void main(String[] args) {
+    Node<String> node1 = new Node<>("banana");
+    Node<String> node2 = new Node<>("mango");
+    Node<String> node3 = new Node<>("kiwi");
+
+    node1.next = node2;
+    node2.next = node3;
+
+    // banana -> mango -> kiwi
+
+    System.out.println(ECSource.getNodeValue(node1, 1));
+  }
+}
---
+++ Source.java
@@ -0,0 +1,33 @@
+class Node<T> {
+    T val;
+    Node<T> next;
+    
+    public Node(T val) {
+      this.val = val;
+      this.next = null;
+    }
+  }
+  
+  class Source {
+    public static <T> boolean linkedListFind(Node<T> head, T target) {
+      // todo
+      return false;
+    }
+    
+    public static void main(String[] args) {
+        Node<String> a = new Node<>("a");
+        Node<String> b = new Node<>("b");
+        Node<String> c = new Node<>("c");
+        Node<String> d = new Node<>("d");
+
+        a.next = b;
+        b.next = c;
+        c.next = d;
+
+        // a -> b -> c -> d
+
+        System.out.println(Source.linkedListFind(a, "c"));
+        
+    }
+
+  }
